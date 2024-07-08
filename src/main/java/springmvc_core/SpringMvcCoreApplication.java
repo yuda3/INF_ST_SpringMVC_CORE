b2b166a03e7614f3015a772e4d9861cb22eafee9
yuda3 <yuda3@hotmail.com>
2024-07-08 17:09:27 +0900

    인프런 김영한 스프링MVC  웹 개발 핵심기술 시작 -20240708

---
+++ src/main/java/springmvc_core/SpringMvcCoreApplication.java
@@ -0,0 +1,13 @@
+package springmvc_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/springmvc_core/SpringMvcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package springmvc_core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
