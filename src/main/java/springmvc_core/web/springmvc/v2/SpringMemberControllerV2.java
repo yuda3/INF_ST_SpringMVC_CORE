31f6ba52056dc125e1a32834463e80997035ffa3
yuda3 <yuda3@hotmail.com>
2024-07-14 07:26:40 +0900

    스프링 mvc V2 추가

---
+++ src/main/java/springmvc_core/web/springmvc/v2/SpringMemberControllerV2.java
@@ -0,0 +1,39 @@
+package springmvc_core.web.springmvc.v2;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import springmvc_core.domain.memeber.Member;
+import springmvc_core.domain.memeber.MemberRepository;
+
+import java.util.List;
+
+@RequestMapping("/springmvc/v2/members")
+public class SpringMemberControllerV2 {
+
+    private final MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/new-form")
+    public ModelAndView newForm() {
+        return new ModelAndView("new-form");
+    }
+
+    @RequestMapping("/save")
+    public ModelAndView save(HttpServletRequest request) {
+        String username = request.getParameter("username");
+        int age = Integer.parseInt(request.getParameter("age"));
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+        ModelAndView mav = new ModelAndView("save-result");
+        mav.addObject("member", member);
+        return mav;
+    }
+
+    @RequestMapping
+    public ModelAndView members() {
+        List<Member> members = memberRepository.findAll();
+        ModelAndView mav = new ModelAndView("members");
+        mav.addObject("members", members);
+        return mav;
+    }
+}
