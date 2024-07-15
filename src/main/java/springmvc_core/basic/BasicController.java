620e0024d01bfef789537f2b64346e2c8ab7ef82
yuda3 <yuda3@hotmail.com>
2024-07-15 21:38:39 +0900

    Url Link

--- src/main/java/springmvc_core/basic/BasicController.java
+++ src/main/java/springmvc_core/basic/BasicController.java
@@ -76,6 +76,13 @@ public class BasicController {
         return "basic/date";
     }
 
+    @GetMapping("/link")
+    public String link(Model model) {
+        model.addAttribute("param1", "data1");
+        model.addAttribute("param2", "data2");
+        return "basic/link";
+    }
+
 
     @Component("helloBean")
     static class HelloBean{
