836031d36495f794f156775a728bf44684b43fd1
yuda3 <yuda3@hotmail.com>
2024-07-15 21:22:53 +0900

    SpringEl

--- src/main/java/springmvc_core/basic/BasicController.java
+++ src/main/java/springmvc_core/basic/BasicController.java
@@ -1,10 +1,13 @@
 package springmvc_core.basic;
 
 
+import lombok.Data;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.*;
 
 @Controller
 @RequestMapping("basic")
@@ -20,5 +23,36 @@ public class BasicController {
     public String textUnescaped(Model model){
         model.addAttribute("data", "Hello <b>World!</b>");
         return "basic/text-unescaped";
+    }
+
+    @GetMapping("/variable")
+    public String variable(Model model){
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> list = new ArrayList<>();
+        list.add(userA);
+        list.add(userB);
+
+        Map<String, User> map = new HashMap<>();
+        map.put("userA", userA);
+        map.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("users", list);
+        model.addAttribute("userMap", map);
+
+        return "basic/variable";
+
+    }
+    @Data
+    static class User{
+        private String username;
+        private int age;
+
+        public User(String username, int age) {
+            this.username = username;
+            this.age = age;
+        }
     }
 }
