163c646c0b6e8baf764b9b7de306e2650c52aea3
yuda3 <yuda3@hotmail.com>
2024-07-15 21:30:50 +0900

    Expression Basic Objects

--- src/main/java/springmvc_core/basic/BasicController.java
+++ src/main/java/springmvc_core/basic/BasicController.java
@@ -1,7 +1,11 @@
 package springmvc_core.basic;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import lombok.Data;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -45,6 +49,34 @@ public class BasicController {
         return "basic/variable";
 
     }
+    /*
+    Lower than Spring 3.0
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        return "basic/basic-objects";
+    }
+    */
+
+    @GetMapping("/basic-objects")
+    public String basicObjects(Model model, HttpServletRequest request,
+                               HttpServletResponse response, HttpSession session) {
+        session.setAttribute("sessionData", "Hello Session");
+        model.addAttribute("request", request);
+        model.addAttribute("response", response);
+        model.addAttribute("servletContext", request.getServletContext());
+        return "basic/basic-objects";
+    }
+
+    @Component("helloBean")
+    static class HelloBean{
+        public String hello(String data){
+            return "Hello " + data;
+        }
+    }
+
+
     @Data
     static class User{
         private String username;
