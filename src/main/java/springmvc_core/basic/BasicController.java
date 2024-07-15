31de6e455c12bd1574b46061d07d9b4092ed386f
yuda3 <yuda3@hotmail.com>
2024-07-15 22:25:46 +0900

    javascript

--- src/main/java/springmvc_core/basic/BasicController.java
+++ src/main/java/springmvc_core/basic/BasicController.java
@@ -139,6 +139,13 @@ public class BasicController {
         return "basic/block";
     }
 
+    @GetMapping("/javascript")
+    public String javascript(Model model) {
+        model.addAttribute("user", new User("userA", 10));
+        addUsers(model);
+        return "basic/javascript";
+    }
+
     @Data
     static class User{
         private String username;
